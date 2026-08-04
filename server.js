e520c95b51701d3154a5b4af2830d25b83eb64d3
luthfyaatika-stack <luthfyaatika@smpn5po.sch.id>
2026-08-04 12:57:23 +0700

    tambahan kategori

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const exspress = require('express');
 const app = exspress();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database Glowlist');
+    }
+});
 const PORT = 3001;
 
 app.use(exspress.json());
