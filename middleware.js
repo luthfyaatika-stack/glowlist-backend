f29ea4b61d18af72b06e09d7693f70621a407eb3
luthfyaatika-stack <luthfyaatika@smpn5po.sch.id>
2026-09-25 08:40:39 +0700

    update glowlist

--- middleware.js
+++ middleware.js
@@ -1,21 +1,32 @@
 const jwt = require('jsonwebtoken');
+
 const secretKey = 'glowlistrahasia';
 
 const authJWT = (req, res, next) => {
-    const token = req.header('Authorization');
+    const authHeader = req.header('Authorization');
 
-    if (token) {
-        const auth = token.split(' ')[1];
-        jwt.verify(auth, secretKey, (err, user) => {
-            if (err) {
-                return res.status(403).json({ message: 'Token tidak valid' });
-            }
-            req.user = user;
-            next();
+    if (!authHeader) {
+        return res.status(401).json({
+            message: 'Silahkan login terlebih dahulu'
         });
-    } else {
-        res.status(401).json({ message: 'silahkan login terlebih dahulu '})
     }
+
+    const token = authHeader.startsWith('Bearer ')
+        ? authHeader.split(' ')[1]
+        : authHeader;
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+            console.log('JWT ERROR:', err.message);
+
+            return res.status(403).json({
+                message: 'Token tidak valid'
+            });
+        }
+
+        req.user = user;
+        next();
+    });
 };
 
 module.exports = authJWT;
